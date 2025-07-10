69f8bb02292c9d7a305166ef85122b535588d908
alejandro_mateus <mmateus@copaair.com>
2025-07-09 21:02:26 -0500

    Add initial implementation of user management features

---
+++ src/main/java/com/backend_user_service/backend/model/AppUser.java
@@ -0,0 +1,59 @@
+package com.backend_user_service.backend.model;
+
+import javax.persistence.*;
+
+@Entity
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    public AppUser() {
+    }
+
+    public AppUser(Long id, String username, String email, boolean active) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
