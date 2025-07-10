84b19564b6dcd60a4bbd83492f5754afaaea38bf
alejandro_mateus <mmateus@copaair.com>
2025-07-09 21:03:03 -0500

    Add user controller and application configuration

---
+++ src/main/java/com/backend_user_service/backend/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.backend_user_service.backend.controller;
+
+import com.backend_user_service.backend.model.AppUser;
+import com.backend_user_service.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user) {
+        AppUser savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestBody AppUser user) {
+        String message = userService.deleteUser(user);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppUser> getUserById(@PathVariable String id) {
+        Optional<AppUser> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppUser>> getAllUsers() {
+        List<AppUser> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
